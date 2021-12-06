03306f8bc5223e312d88a957b9d6346b400a137a
Johannes Henninger <jhenninger@users.noreply.github.com>
2021-12-06 11:05:47 +0100

    Day 6

---
+++ day06/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let input = include_str!("../input").trim();
+    let mut timers = vec![0usize; 9];
+
+    for fish in input.split(',') {
+        let timer: usize = fish.parse().unwrap();
+        timers[timer] += 1;
+    }
+
+    println!(
+        "Part 1: {}\nPart 2: {}",
+        generations(80, &mut timers),
+        generations(256 - 80, &mut timers),
+    );
+}
+
+fn generations(n: usize, timers: &mut [usize]) -> usize {
+    for _ in 0..n {
+        timers.rotate_left(1);
+        timers[6] += timers[8];
+    }
+    timers.iter().sum()
+}
