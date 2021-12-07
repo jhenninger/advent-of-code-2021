44fbf261e39b3b1ca8b4f01e5ada05b5af9417b0
Johannes Henninger <jhenninger@users.noreply.github.com>
2021-12-07 14:31:30 +0100

    Day 7

---
+++ day07/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut positions: Vec<i32> = include_str!("../input")
+        .trim()
+        .split(',')
+        .map(|p| p.parse().unwrap())
+        .collect();
+    positions.sort_unstable();
+
+    let median = positions[positions.len() / 2];
+
+    let part_1: i32 = positions.iter().map(|p| (median - p).abs()).sum();
+
+    let part_2: i32 = (positions[0]..=positions[positions.len() - 1])
+        .map(|target| {
+            positions
+                .iter()
+                .map(|p| {
+                    let n = (target - p).abs();
+                    n * (n + 1) / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap();
+
+    println!("Part 1: {}\nPart 2: {}", part_1, part_2);
+}
