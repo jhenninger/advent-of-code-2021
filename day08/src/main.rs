04ee91b2886a5b1930c9c02bb48dd24e5ae5213d
Johannes Henninger <jhenninger@users.noreply.github.com>
2021-12-08 23:56:09 +0100

    Day 8: condense everything into one unreadable iterator chain
    Inspired by https://www.reddit.com/r/adventofcode/comments/rbj87a/comment/hnoyy04

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,113 +1,50 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input").trim();
 
-    let mut displays: Vec<(Vec<String>, Vec<String>)> = Vec::new();
-    for line in input.lines() {
-        let (a, b) = line.split_once(" | ").unwrap();
-        displays.push((
-            a.split_ascii_whitespace()
-                .map(|s| {
-                    let mut x: Vec<_> = s.chars().collect();
-                    x.sort_unstable();
-                    x.iter().collect()
-                })
-                .collect(),
-            b.split_ascii_whitespace()
-                .map(|s| {
-                    let mut x: Vec<_> = s.chars().collect();
-                    x.sort_unstable();
-                    x.iter().collect()
-                })
-                .collect(),
-        ));
-    }
+    let (part_1, part_2): (u32, u32) = input.lines().fold((0, 0), |acc, l| {
+        let (patterns, output) = l.split_once(" | ").unwrap();
 
-    let mut sum = 0;
+        let one: HashSet<char> = patterns
+            .split_whitespace()
+            .find(|s| s.len() == 2)
+            .map(|s| s.chars().collect())
+            .unwrap();
 
-    for d in displays.iter() {
+        let four: HashSet<char> = patterns
+            .split_whitespace()
+            .find(|s| s.len() == 4)
+            .map(|s| s.chars().collect())
+            .unwrap();
 
-        let mut mapping: HashMap<&str, u8> = HashMap::new();
-        let mut stuff: HashSet<String> = d.0.iter().cloned().collect();
+        let (p1, p2) = output.split_whitespace().fold((0, 0), |(p1, p2), d| {
+            let segments: HashSet<char> = d.chars().collect();
+            
+            // identify each digit by its segment count and its intersection with of 1 and 4
+            let digit = match (
+                segments.len(),
+                segments.intersection(&four).count(),
+                segments.intersection(&one).count(),
+            ) {
+                (2, _, _) => 1,
+                (3, _, _) => 7,
+                (4, _, _) => 4,
+                (7, _, _) => 8,
+                (5, 2, _) => 2,
+                (5, 3, 1) => 5,
+                (5, 3, 2) => 3,
+                (6, 4, _) => 9,
+                (6, 3, 1) => 6,
+                (6, 3, 2) => 0,
+                _ => panic!(),
+            };
 
-        let one = stuff.iter().find(|x| x.len() == 2).unwrap().clone();
-        let four = stuff.iter().find(|x| x.len() == 4).unwrap().clone();
-        let seven = stuff.iter().find(|x| x.len() == 3).unwrap().clone();
-        let eight = stuff.iter().find(|x| x.len() == 7).unwrap().clone();
+            (p1 + [1, 4, 7, 8].contains(&digit) as u32, p2 * 10 + digit)
+        });
 
-        stuff.remove(&one);
-        stuff.remove(&four);
-        stuff.remove(&seven);
-        stuff.remove(&eight);
+        (acc.0 + p1, acc.1 + p2)
+    });
 
-        mapping.insert(&one, 1);
-        mapping.insert(&four, 4);
-        mapping.insert(&seven, 7);
-        mapping.insert(&eight, 8);
-
-        let three = stuff
-            .iter()
-            .find(|x| x.chars().filter(|&c| !one.contains(c)).count() == 3)
-            .unwrap()
-            .clone();
-
-
-        stuff.remove(&three);
-        mapping.insert(&three, 3);
-
-
-        let six = stuff
-            .iter()
-            .find(|x| x.chars().filter(|&c| !seven.contains(c)).count() == 4)
-            .unwrap()
-            .clone();
-
-        stuff.remove(&six);
-        mapping.insert(&six, 6);
-
-        let zero = stuff
-            .iter()
-            .find(|x| x.chars().filter(|&c| !three.contains(c)).count() == 2)
-            .unwrap()
-            .clone();
-
-        stuff.remove(&zero);
-        mapping.insert(&zero, 0);
-
-        let two = stuff
-            .iter()
-            .find(|x| x.chars().filter(|&c| !four.contains(c)).count() == 3)
-            .unwrap()
-            .clone();
-
-        stuff.remove(&two);
-        mapping.insert(&two, 2);
-
-        let five = stuff
-            .iter()
-            .find(|x| x.chars().filter(|&c| !six.contains(c)).count() == 0)
-            .unwrap()
-            .clone();
-
-        stuff.remove(&five);
-        mapping.insert(&five, 5);
-
-        let nine = stuff.iter().next().unwrap().clone();
-        stuff.remove(&nine);
-        mapping.insert(&nine, 9);
-
-        assert!(stuff.is_empty());
-
-        let mut digit: i32 = 0;
-        for n in &d.1 {
-            digit *= 10;
-            digit += mapping[&n[..]] as i32;
-
-        }
-
-        sum += digit;
-    }
-
-    dbg!(sum);
+    println!("Part 1: {}\nPart 2: {}", part_1, part_2);
 }
