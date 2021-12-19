6977396cc843a6e430df92c30da8f2c714f2cc09
Johannes Henninger <jhenninger@users.noreply.github.com>
2021-12-19 02:26:28 +0100

    Day 18: simplify things

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,128 +1,106 @@
 use std::{iter::Sum, ops::Add};
+use SnailNumber::*;
 
-#[derive(Clone)]
-struct Number(Part, Part);
-
-#[derive(Clone)]
-enum Part {
-    Number(Box<Number>),
+#[derive(Debug, Clone)]
+enum SnailNumber {
+    Pair(Box<SnailNumber>, Box<SnailNumber>),
     Literal(u32),
 }
 
-impl Part {
-    fn leftmost(&mut self) -> &mut u32 {
-        match self {
-            Part::Literal(n) => n,
-            Part::Number(n) => n.0.leftmost(),
-        }
-    }
-
-    fn rightmost(&mut self) -> &mut u32 {
-        match self {
-            Part::Literal(n) => n,
-            Part::Number(n) => n.1.rightmost(),
-        }
-    }
-
+impl SnailNumber {
     fn explode(&mut self, depth: usize) -> (bool, Option<u32>, Option<u32>) {
         match self {
-            Part::Literal(_) => (false, None, None),
-            Part::Number(n) => {
-                if depth == 4 {
-                    match **n {
-                        Number(Part::Literal(left), Part::Literal(right)) => {
-                            *self = Part::Literal(0);
-                            (true, Some(left), Some(right))
-                        }
-                        _ => panic!("expected two literals"),
-                    }
-                } else {
-                    n.explode(depth)
-                }
+            Literal(_) => (false, None, None),
+            Pair(left, right) if depth == 4 => {
+                let result = (true, left.literal(), right.literal());
+                *self = Literal(0);
+                result
             }
+            Pair(left, right) => {
+                let (change, l, mut r) = left.explode(depth + 1);
+                if let Some(v) = r.take() {
+                    right.add_left(v);
+                }
+
+                if change {
+                    return (change, l, r);
+                }
+
+                let (change, mut l, r) = right.explode(depth + 1);
+
+                if let Some(v) = l.take() {
+                    left.add_right(v);
+                }
+
+                (change, l, r)
+            }
+        }
+    }
+
+    fn literal(&self) -> Option<u32> {
+        match self {
+            Literal(v) => Some(*v),
+            _ => None,
         }
     }
 
     fn split(&mut self) -> bool {
         match self {
-            Part::Literal(n) if *n >= 10 => {
-                let num = Number(
-                    Part::Literal(*n / 2),
-                    Part::Literal((*n as f32 / 2.0).ceil() as u32),
+            Pair(left, right) => left.split() || right.split(),
+            Literal(n) if *n >= 10 => {
+                *self = Pair(
+                    Box::new(Literal(*n / 2)),
+                    Box::new(Literal((*n as f32 / 2.0).ceil() as u32)),
                 );
-                *self = Part::Number(Box::new(num));
                 true
             }
-            Part::Number(n) => n.split(),
             _ => false,
         }
     }
 
+    fn add_left(&mut self, value: u32) {
+        match self {
+            Literal(n) => *n += value,
+            Pair(left, _) => left.add_left(value),
+        }
+    }
+
+    fn add_right(&mut self, value: u32) {
+        match self {
+            Literal(n) => *n += value,
+            Pair(_, right) => right.add_right(value),
+        }
+    }
+
     fn magnitude(&self) -> u32 {
         match self {
-            Part::Number(n) => n.magnitude(),
-            Part::Literal(n) => *n,
+            Literal(n) => *n,
+            Pair(left, right) => 3 * left.magnitude() + 2 * right.magnitude(),
         }
     }
 }
 
-impl Number {
-    fn reduce(&mut self) {
-        while self.explode(0).0 || self.split() {}
-    }
-
-    fn explode(&mut self, depth: usize) -> (bool, Option<u32>, Option<u32>) {
-        let (changed, left, mut right) = self.0.explode(depth + 1);
-
-        if let Some(value) = right.take() {
-            *self.1.leftmost() += value;
-        }
-
-        if changed {
-            return (true, left, right);
-        }
-
-        let (changed, mut left, right) = self.1.explode(depth + 1);
-
-        if let Some(value) = left.take() {
-            *self.0.rightmost() += value;
-        }
-
-        return (changed, left, right);
-    }
-
-    fn split(&mut self) -> bool {
-        self.0.split() || self.1.split()
-    }
-
-    fn magnitude(&self) -> u32 {
-        3 * self.0.magnitude() + 2 * self.1.magnitude()
-    }
-}
-
-impl Add for Number {
-    type Output = Number;
+impl Add for SnailNumber {
+    type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut new = Number(Part::Number(Box::new(self)), Part::Number(Box::new(rhs)));
-        new.reduce();
-        new
+        let mut sum = Pair(Box::new(self), Box::new(rhs));
+        while sum.explode(0).0 || sum.split() {}
+        sum
     }
 }
 
-impl Sum for Number {
+impl Sum for SnailNumber {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.reduce(|a, b| a + b)
-            .unwrap_or(Number(Part::Literal(0), Part::Literal(0)))
+        iter.reduce(|a, b| a + b).unwrap_or(Literal(0))
     }
 }
 
 fn main() {
     let input = include_str!("../input").trim();
+    let numbers: Vec<SnailNumber> = input.lines().map(parse).collect();
 
-    let numbers: Vec<Number> = input.lines().map(parse_number).collect();
-
-    let part_1 = numbers.iter().cloned().sum::<Number>().magnitude();
+    let part_1 = numbers.iter().cloned().sum::<SnailNumber>().magnitude();
 
     let part_2 = numbers
         .iter()
@@ -137,32 +115,25 @@ fn main() {
     println!("Part 1: {}\nPart 2: {}", part_1, part_2);
 }
 
-fn parse_number(s: &str) -> Number {
-    let s = &s[1..s.len() - 1];
-
-    let mut depth = 0;
-    let (left, right) = s
-        .split_once(|c| match c {
-            '[' => {
-                depth += 1;
-                false
-            }
-            ']' => {
-                depth -= 1;
-                false
-            }
-            ',' if depth == 0 => true,
-            _ => false,
-        })
-        .unwrap();
-
-    Number(parse_part(left), parse_part(right))
-}
-
-fn parse_part(s: &str) -> Part {
+fn parse(s: &str) -> SnailNumber {
     if s.starts_with('[') {
-        Part::Number(Box::new(parse_number(s)))
+        let mut depth = 0;
+        let (left, right) = s[1..s.len() - 1]
+            .split_once(|c| match c {
+                '[' => {
+                    depth += 1;
+                    false
+                }
+                ']' => {
+                    depth -= 1;
+                    false
+                }
+                ',' if depth == 0 => true,
+                _ => false,
+            })
+            .unwrap();
+        Pair(Box::new(parse(left)), Box::new(parse(right)))
     } else {
-        Part::Literal(s.parse().unwrap())
+        Literal(s.parse().unwrap())
     }
 }
