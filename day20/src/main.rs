76d7c1a6f07fdf931f5506f25b5086b88c69a88e
Johannes Henninger <jhenninger@users.noreply.github.com>
2021-12-20 13:57:26 +0100

    Day 20: cleanup

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,58 +1,56 @@
 use std::{collections::HashSet, mem};
 
 fn main() {
-    let input = include_str!("../input");
+    let input = include_str!("../input").trim();
+    let (enhancement, image) = input.split_once("\n\n").unwrap();
 
-    let (a, b) = input.split_once("\n\n").unwrap();
+    let enhancement: Vec<bool> = enhancement.chars().map(|c| c == '#').collect();
 
-    let e: Vec<_> = a.chars().map(|c| c == '#').collect();
-
-    let mut img: HashSet<(i32, i32)> = b
+    let mut image: HashSet<(i32, i32)> = image
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
-            line.chars().enumerate().filter_map(move |(x, c)| {
-                if c == '#' {
-                    Some((x as i32, y as i32))
-                } else {
-                    None
-                }
+            line.chars().enumerate().filter_map(move |(x, c)| match c {
+                '#' => Some((x as i32, y as i32)),
+                _ => None,
             })
         })
         .collect();
 
+    let mut part_1 = 0;
+
     let mut next = HashSet::new();
 
     for i in 0..50 {
-        let min_x = img.iter().map(|p| p.0).min().unwrap();
-        let max_x = img.iter().map(|p| p.0).max().unwrap();
-        let min_y = img.iter().map(|p| p.1).min().unwrap();
-        let max_y = img.iter().map(|p| p.1).max().unwrap();
+        if i == 2 {
+            part_1 = image.len();
+        }
 
-        for y in min_y-1..=max_y+1 {
-            for x in min_x-1..=max_x+1 {
+        let min_x = image.iter().map(|p| p.0).min().unwrap();
+        let max_x = image.iter().map(|p| p.0).max().unwrap();
+        let min_y = image.iter().map(|p| p.1).min().unwrap();
+        let max_y = image.iter().map(|p| p.1).max().unwrap();
+
+        for y in min_y - 1..=max_y + 1 {
+            for x in min_x - 1..=max_x + 1 {
                 let mut idx = 0;
-                for dy in -1..=1 {
-                    for dx in -1..=1 {
-                        let nx = x + dx;
-                        let ny = y + dy;
-                        idx <<= 1;
-                        idx |= ((i % 2 == 1
-                            && (nx < min_x || nx > max_x || ny < min_y || ny > max_y))
-                            || img.contains(&(x + dx, y + dy)))
-                            as usize;
+                for ny in y - 1..=y + 1 {
+                    for nx in x - 1..=x + 1 {
+                        let outside_and_active =
+                            i % 2 == 1 && (nx < min_x || nx > max_x || ny < min_y || ny > max_y);
+                        idx = idx << 1 | (outside_and_active || image.contains(&(nx, ny))) as usize;
                     }
                 }
 
-                if e[idx] {
+                if enhancement[idx] {
                     next.insert((x, y));
                 }
             }
         }
 
-        mem::swap(&mut img, &mut next);
+        mem::swap(&mut image, &mut next);
         next.clear();
     }
 
-    dbg!(img.len());
+    println!("Part 1: {}\nPart 2: {}", part_1, image.len());
 }
